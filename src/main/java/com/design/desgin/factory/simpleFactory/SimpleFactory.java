cf705a2dd3a1598014072769815dccaadba09e2c
Ago <xiasamaart@outlook.com>
2020-03-02 23:03:50 +0800

    简单的工厂模式实现

---
+++ src/main/java/com/design/desgin/factory/simpleFactory/SimpleFactory.java
@@ -0,0 +1,11 @@
+package com.design.desgin.factory.simpleFactory;
+
+/**
+ * @author ：Ago
+ * @date ：Created in 2020/3/2 23:02
+ * @description：简单工厂模式
+ * @modified By：
+ * @version: 1.0$
+ */
+public class SimpleFactory {
+}
